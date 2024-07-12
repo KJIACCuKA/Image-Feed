436b6f016f49badfb4cfb808db6b5954502606af
Nikita1380 <160261662+Nikita1380@users.noreply.github.com>
2024-07-12 14:26:35 +0300

    sprint_10

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let AccessKey = "4fOOKt0scYT_ke4SWfEQPvSGRCFx9k7Vu1oRKfG3zxo"
+let SecretKey = "Lk4UqcoKT3VS8WN84twE1yfWFR5IbbQctQ1t0d65nlA"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
