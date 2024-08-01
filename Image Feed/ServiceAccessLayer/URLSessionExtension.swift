a9d7f2fd17a23258c97df76de386917b62c4593e
Nikita1380 <nik138013.yandex.ru>
2024-08-01 15:17:14 +0300

    Add logs for Errors
    Добавлены логи для определения ошибок. Список кода ошибок обновлен.

--- Image Feed/ServiceAccessLayer/URLSessionExtension.swift
+++ Image Feed/ServiceAccessLayer/URLSessionExtension.swift
@@ -5,6 +5,11 @@ enum NetworkError: Error {
     case httpStatusCode(Int)
     case urlRequestError(Error)
     case urlSessionError
+    case badRequest
+    case unauthorized
+    case forbidden
+    case notFound
+    case serverError
 }
 
 extension URLSession {
@@ -20,14 +25,35 @@ extension URLSession {
         
         let task = dataTask(with: request, completionHandler: { data, response, error in
             if let data = data, let response = response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                if 200 ..< 300 ~= statusCode {
+                switch statusCode {
+                case 200...299:
+                    print("DEBUG:", "Received success HTTP status code: \(statusCode)")
                     fulfillCompletionOnTheMainThread(.success(data))
-                } else {
+                case 400:
+                    print("ERROR:", "Bad request \(statusCode)")
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.badRequest))
+                case 401:
+                    print("ERROR:", "Unauthorized \(statusCode) - Invalid Access Token")
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.unauthorized))
+                case 403:
+                    print("ERROR:", "Forbidden \(statusCode)")
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.forbidden))
+                case 404:
+                    print("ERROR:", "Not Found \(statusCode)")
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.notFound))
+                    
+                case 500, 503:
+                    print("ERROR:", "Internal Server Error \(statusCode)")
+                    fulfillCompletionOnTheMainThread(.failure(NetworkError.serverError))
+                default:
+                    print("ERROR:", "HTTP status code: \(statusCode)")
                     fulfillCompletionOnTheMainThread(.failure(NetworkError.httpStatusCode(statusCode)))
                 }
             } else if let error = error {
+                print("ERROR:", error.localizedDescription)
                 fulfillCompletionOnTheMainThread(.failure(NetworkError.urlRequestError(error)))
             } else {
+                print("ERROR: Unknown error")
                 fulfillCompletionOnTheMainThread(.failure(NetworkError.urlSessionError))
             }
         })
