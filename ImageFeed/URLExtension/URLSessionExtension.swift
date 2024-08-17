b9c88baa0a6b74124b2711ede9444e0c9d80cab0
Nikita1380 <160261662+Nikita1380@users.noreply.github.com>
2024-08-17 11:32:39 +0300

    fix URL force unwrap

--- ImageFeed/URLExtension/URLSessionExtension.swift
+++ ImageFeed/URLExtension/URLSessionExtension.swift
@@ -71,3 +71,13 @@ extension URLSession {
     }
 }
 
+extension URL {
+    init(staticString string: StaticString) {
+        guard let url = URL(string: "\(string)") else {
+            preconditionFailure("Invalid static URL string: \(string)")
+        }
+
+        self = url
+    }
+}
+
